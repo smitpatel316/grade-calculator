ec3233baa9af7db0cfe2e783d7faf94c34c992bd
Smit Patel <smitpatel316@gmail.com>
2019-05-19 16:08:21 -0400

    added firebase to properties

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCKFYfQ4JYF4chKoMxe12SI6gxOXg_gw3k',
+    authDomain: 'grade-calculator-d94df.firebaseapp.com',
+    databaseURL: 'https://grade-calculator-d94df.firebaseio.com',
+    projectId: 'grade-calculator-d94df',
+    storageBucket: 'grade-calculator-d94df.appspot.com',
+    messagingSenderId: '792257153685'
+  }
 };
